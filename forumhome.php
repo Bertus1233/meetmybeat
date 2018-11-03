5ae2aa819e48040ef34198f004747fabbc54b0c7
Bertus1233 <44726438+Bertus1233@users.noreply.github.com>
2018-11-03 14:49:23 +0100

    Add files via upload

---
+++ forumhome.php
@@ -0,0 +1,23 @@
+<?php
+//include 'dbconnect.php';
+include 'header2.php';
+
+
+
+
+
+echo '<tr>';
+    echo '<td>Welkom op het forum, hier kan u een onderwerp aanmaken of 
+	reageren op een ander onderwerp ' . "<br>";               
+echo '</tr>';
+
+
+
+
+
+
+
+
+
+include 'footer2.php';
+?>
