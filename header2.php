e45000f09e6fe49d86b0353ebd3d863394535ce0
Bertus1233 <44726438+Bertus1233@users.noreply.github.com>
2018-11-05 11:57:44 +0100

    Add files via upload

---
+++ header2.php
@@ -0,0 +1,20 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="beschrijving" content="Korte beschrijving." />
+    <meta name="keywords" content="put, keywords, here" />
+    <title>Meet my beat forum</title>
+    <link rel="stylesheet" href="forumstyle.css" type="text/css">
+</head>
+
+<body>
+
+
+    <div id="wrapper">
+    <div id="menu">
+		<a class="item" href="">Hoofdpagina</a> -
+		<a class="item" href="forumhome.php">Home</a> -
+        <a class="item" href="cat_overzicht.php">Categorieën</a> -
+		<a class="item" href="catagorie.php">Categorie aanmaken</a> -
+		<a class="item" href="forumform.php">Onderwerp aanmaken</a>
+	</div>
+        <div id="content">
